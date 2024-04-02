774bcd964286083e03a3410e6c3f868a5800184a
Maxamed Maxamed <20088349@mail.wit.ie>
2024-04-02 18:40:11 +0100

    Add data loading and analysis code

--- assignment.py
+++ assignment.py
@@ -1,3 +1,19 @@
-# This is a new file named assignment.py
+import packaging  # Import the 'packaging' module 
 
-# Add your Python code here
+import pandas as pd 
+  # Import the 'pandas' module
+covid_data = pd.read_csv('covid_data.csv', low_memory=False)  # Load data
+
+# Display the first 5 rows of the data
+print(covid_data.head())
+
+
+# Display the last 5 rows of the data
+print(covid_data.tail(5))
+
+
+# location in Ireland
+location = 'Ireland' # Define the location
+variable = 'total_vaccinations' # Define the variable
+# Display the description statistics for the variable for the location n
+print(covid_data[covid_data['location'] == location][[variable]].describe())
